2f99118a9f5f536553fbb7a54065dd749ff066ba
Rostyslav <noviyrost@gmail.com>
2022-07-15 11:15:35 +0300

    add just validate

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 // search
-document.querySelector(".header__search").addEventListener("click", function() {
-    document.querySelector(".header-form").classList.add("header-form__active");
-    this.classList.add("active");
-  })
+// document.querySelector(".header__search").addEventListener("click", function() {
+//     document.querySelector(".header-form").classList.add("header-form__active");
+//     this.classList.add("active");
+//   })
 
-  document.addEventListener("click", function(e) {
-    let target = e.target;
-    let form = document.querySelector(".header-form");
+document.querySelector(".header__search").addEventListener("click", function() {
+  document.querySelector(".header-form").classList.add("header-form__active");
+})
+document.querySelector(".header-form__btn").addEventListener("click", function() {
+  document.querySelector(".header-form").classList.remove("header-form__active");
+})
+
+
+document.querySelector(".header__search").addEventListener("click", function() {
+  document.querySelector(".header__search").classList.add("active");
+})
+document.querySelector(".header-form__btn").addEventListener("click", function() {
+  document.querySelector(".header__search").classList.remove("active");
+})
+
+
+  // document.addEventListener("click", function(e) {
+  //   let target = e.target;
+  //   let form = document.querySelector(".header-form");
   
-    if (!target.closest(".header__container")) {
-    form.classList.remove("header-form__active");
-      form.querySelector("input").value = "";
-      document.querySelector(".header__search").classList.remove("active")
-    }
-  })
+  //   if (!target.closest(".header__container")) {
+  //   form.classList.remove("header-form__active");
+  //     form.querySelector("input").value = "";
+  //     document.querySelector(".header__search").classList.remove("active")
+  //   }
+  // })
 
 //   burger
 
@@ -32,20 +48,40 @@ document.querySelector(".burger__btn").addEventListener("click", function() {
   document.querySelector("body").classList.remove("body-burger");
 })
 
-// form 
+// form  just mail
 
 new JustValidate('.about__form', {
-    rules: {
-        },
-        mail: {
-            required: true,
-            email: true
-        }
-    }
+  rules: {
+      },
+      mail: {
+          required: true,
+          // email: true
+      }
+  }
 )
 
 // map 
 
-// document.querySelector(".contacts__x").addEventListener("click", function() {
-//   document.querySelector(".contacts__connect").classList.add("contacts__connect-active");
-// })
+document.querySelector(".contacts__x").addEventListener("click", function() {
+  document.querySelector(".contacts__connect").classList.add("contacts__connect-active");
+})
+document.querySelector(".contacts__o").addEventListener("click", function() {
+  document.querySelector(".contacts__connect").classList.remove("contacts__connect-active");
+})
+
+// form name mail tell
+
+new JustValidate('.contacts__form', {
+  rules: {
+      name: {
+          // required: true,
+          minLength: 2,
+          maxLength: 10
+      },
+      mail: {
+          required: true,
+          email: true
+      }
+  }
+
+})
